fd195a31cb5a7e698c23b3f46af220d1cb2d7ad4
Zebraholic <rctavares@rogers.com>
2015-02-22 10:17:25 -0500

    Added acts_as_votable like feature.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
-	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@pins = Pin.all.order("created_at DESC")
@@ -14,6 +15,7 @@ class PinsController < ApplicationController
 
 	def create
 		@pin = current_user.pins.build(pin_params)
+
 		if @pin.save
 			redirect_to @pin, notice: "Pin was successfully created"
 		else
@@ -37,6 +39,11 @@ class PinsController < ApplicationController
 		redirect_to root_path
 	end
 
+	def upvote
+		@pin.upvote_by current_user
+		redirect_to :back
+	end
+
 	private
 
 	def pin_params
@@ -46,4 +53,5 @@ class PinsController < ApplicationController
 	def find_pin
 		@pin = Pin.find(params[:id])
 	end
+
 end
